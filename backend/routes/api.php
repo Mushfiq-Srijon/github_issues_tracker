9938a833975e1c4a475ec28177de7db8b677d690
Mushfiq-Srijon <mushfiq.srijon@gmail.com>
2026-09-19 15:13:37 +0600

    Connect dashboard to Laravel API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IssueController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -11,6 +12,8 @@ Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
     Route::get('/user', [AuthController::class, 'user']);
+
+    Route::get('/dashboard', [DashboardController::class, 'index']);
 
     Route::get('/issues', [IssueController::class, 'index']);
     Route::post('/issues', [IssueController::class, 'store']);
